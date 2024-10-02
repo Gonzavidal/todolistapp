75f82395996a7425ef4ab9e70341a240a07110ce
Gonzavidal <gvidal1992@gmail.com>
2024-10-02 12:55:52 -0300

    Adding viewmodel

--- app/src/main/java/com/example/todolistapp/domain/usecase/GetTasksUseCase.kt
+++ app/src/main/java/com/example/todolistapp/domain/usecase/GetTasksUseCase.kt
@@ -3,7 +3,7 @@ package com.example.todolistapp.domain.usecase
 import com.example.todolistapp.data.model.Task
 import com.example.todolistapp.data.repository.TaskRepository
 
-class GetTaskUseCase(private val repository: TaskRepository) {
+class GetTasksUseCase(private val repository: TaskRepository) {
     suspend operator fun invoke(task: Task) {
         repository.getTasks()
     }
