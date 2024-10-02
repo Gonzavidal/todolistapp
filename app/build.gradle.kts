b226ae880d9d8f29d8ba0841e49e9ede8a6efe04
Gonzavidal <gvidal1992@gmail.com>
2024-10-02 19:55:02 -0300

    Fixing Koin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation(libs.androidx.lifecycle.runtime.ktx.v286)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
@@ -65,8 +65,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.2")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.6")
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 
     implementation("androidx.navigation:navigation-compose:2.8.1")
 
